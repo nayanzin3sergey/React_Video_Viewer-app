e190e3cfcd85e2c74d9e1f2d838d459465d8b57d
nayanzin3sergey <nayanzin.sergey3@gmail.com>
2018-10-19 13:33:37 -0400

    creating Navbar.js.  Added 3 click events: About, Videos, and Photos.  Correlates to the 3 Navbar buttons.

--- video_viewer_front_end/src/components/Navbar.js
+++ video_viewer_front_end/src/components/Navbar.js
@@ -1,1 +1,60 @@
+mport React, { Component } from 'react';
 
+class NavBar extends Component {
+	constructor(props) {
+		super(props);
+		this.state.about = {ClickedAbout: false}
+		this.state.videos = {ClickedVideos: false}
+		this.state.photos = {ClickedPhotos: false}
+
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClickAbout() {
+		this.setState(state => ({
+			ClickedAbout: !state.about
+		}));
+		console.log(this.state)
+	}
+
+
+	handleClickVideos() {
+		this.setState(state => ({
+			ClickedVideos: !state.videos
+		}));
+		console.log(this.state)
+	}
+
+
+	handleClickPhotos() {
+		this.setState(state => ({
+			ClickedPhotos: !state.photos
+		}));
+		console.log(this.state)
+	}
+
+	  render() {
+	    return <button 
+	    			className="button-navbar" 
+	    			id="about" 
+	    			onClick={this.props.handleClick} 
+	    			style= {this.styleNavbar}
+	   		   > About </button>;
+
+	   		   <button 
+	    			className="button-navbar" 
+	    			id="videos" 
+	    			onClick={this.props.handleClick} 
+	    			style= {this.styleNavbar}
+	   		   > Videos </button>;
+
+	   		   <button 
+	    			className="button-navbar" 
+	    			id="photos" 
+	    			onClick={this.props.handleClick} 
+	    			style= {this.styleNavbar}
+	   		   > Photos </button>;
+  }
+}
+
+export default NavBar
