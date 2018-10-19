fd2c129f8038da9bbaf6fd353e723a234228dde6
nayanzin3sergey <nayanzin.sergey3@gmail.com>
2018-10-19 12:41:28 -0400

    Moved Sign In button component into header under React logo.  Added handleClick to display Signing in upon clikcing button

--- video_viewer_front_end/src/App.js
+++ video_viewer_front_end/src/App.js
@@ -8,24 +8,27 @@ import PhotoGallery from './components/PhotoGallery'
 
 class App extends Component {
   render() {
+
+    function clicked(){
+      console.log("CLICKED")
+    };
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          < SignIn />
+
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
           </a>
         </header>
+
         < Welcome />
-        < SignIn />
         < ViewVideo />
         < PhotoGallery />
 
--- video_viewer_front_end/src/components/SignIn.js
+++ video_viewer_front_end/src/components/SignIn.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 
 class SignIn extends Component {
-  render() {
-    return <button className="sign-in"> Sign In </button>;
+	constructor(props) {
+		super(props);
+		this.state = {isSigningIn: true}
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick() {
+		this.setState(state => ({
+			isSigningIn: !state.isSigningIn
+		}));
+	}
+
+	  render() {
+	    return <button onClick={this.props.handleClick}> {this.state.isSigningIn ? 'Sign In' : 'Signing In...'} </button>;
   }
 }
 
